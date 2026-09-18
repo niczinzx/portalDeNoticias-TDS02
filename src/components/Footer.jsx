121ef6542c029b2d5f0a96d313a930de66fd341f
niczinzx <nicktube030@gmail.com>
2026-09-18 11:40:17 -0300

    Complementando o rodapé

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,11 @@
 export default function Footer() {
     return (
         <footer>
+            <hr />
+            <h3>Rodapé</h3>
+            <p><a href="http://github/niczinzx">Github</a></p>
+            <p><a href="mailto:nicktube030@gmail.com">portaldenoticias@gmail.com</a></p>
+            <p>Portal de Notícias</p>
             <p>© 2026 - Desenvolvido por Nicolas</p>
         </footer>
     )
