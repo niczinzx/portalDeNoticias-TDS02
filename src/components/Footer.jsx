c84a5a052b49b2efb804aabf153e4960c8b70bc7
niczinzx <nicktube030@gmail.com>
2026-09-18 11:45:48 -0300

    Ajustando os links github e email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ export default function Footer() {
         <footer>
             <hr />
             <h3>Rodapé</h3>
-            <p><a href="http://github/niczinzx">Github</a></p>
-            <p><a href="mailto:nicktube030@gmail.com">portaldenoticias@gmail.com</a></p>
+            <p><a href="https://github.com/niczinzx">Github</a></p>
+            <p><a href="mailto:portaldenoticias@gmail.com">portaldenoticias@gmail.com</a></p>
             <p>Portal de Notícias</p>
             <p>© 2026 - Desenvolvido por Nicolas</p>
         </footer>
